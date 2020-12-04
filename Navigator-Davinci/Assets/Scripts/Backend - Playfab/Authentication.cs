fb58bdbd225938bcc093d239df597a8bb9b3d8da
yavuz <yavuzkaand@hotmail.com>
2020-12-04 18:01:54 +0100

    Playfab initialized

---
+++ Navigator-Davinci/Assets/Scripts/Backend - Playfab/Authentication.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab.ClientModels;
+using PlayFab;
+
+public class Authentication : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Login();
+    }
+    public void Login()
+    {
+        var request = new LoginWithEmailAddressRequest { Email = "yavuzkaand@hotmail.com", Password = "poker12345" };
+        PlayFabClientAPI.LoginWithEmailAddress(request, result => {
+            Debug.Log("User " + request.Email + " has logged in");
+        
+        }, error => {
+            Debug.Log(error.GenerateErrorReport());
+        
+        });
+
+    }
+}
