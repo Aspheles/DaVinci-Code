c0a6f80fc21f3a1f4537cbdbd6cb04fcf6bcf5a1
CodeerBeer <99053182@mydavinci.nl>
2020-12-09 15:11:09 +0100

    MenuManager intitialized.
    Made functions leading to certain menu's.

---
+++ Navigator-Davinci/Assets/Scripts/UI/Launcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public void OpenLoginMenu()
+    {
+        MenuManager.instance.OpenMenu("login");
+    }
+
+    public void OpenRegisterMenu()
+    {
+        MenuManager.instance.OpenMenu("register");
+    }
+
+    public void OpenMainMenu()
+    {
+        MenuManager.instance.OpenMenu("main");
+    }
+
+    public void OpenErrorMenu()
+    {
+        MenuManager.instance.OpenMenu("error");
+    }
+
+    public void OpenSuccessMenu()
+    {
+        MenuManager.instance.OpenMenu("success");
+    }
+}
---
+++ Navigator-Davinci/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] Menu[] menus;
+    public static MenuManager instance;
+
+    private void Start()
+    {
+        if (instance != null) return;
+        instance = this;
+    }
+    public void OpenMenu(string menuName)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].menuName == menuName)
+            {
+                menus[i].Open();
+            }
+            else if (menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+    }
+
+    public void OpenMenu(Menu menu)
+    {
+        for(int i = 0; i < menus.Length; i++)
+        {
+            if(menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+        menu.Open();
+    }
+
+
+    public void CloseMenu(Menu menu)
+    {
+        menu.Close();
+    }
+}
