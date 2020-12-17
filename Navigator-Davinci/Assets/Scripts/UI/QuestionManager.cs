f850ac9d91eb49709c20389b9b870e91f416b422
sandergeldof22 <99046589@mydavinci.nl>
2020-12-17 14:23:40 +0100

    Dynamiek toegevoegd

---
+++ Navigator-Davinci/Assets/Scripts/UI/QuestionManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestionManager : MonoBehaviour
+{
+    public TMP_Text questionTitle;
+    public static QuestionManager instance;
+
+
+    private void Start()
+    {
+        instance = this;
+    }
+}
---
+++ Navigator-Davinci/Assets/Scripts/UI/QuestionOverview.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Networking;
+
+
+public class QuestionOverview : MonoBehaviour
+{
+    public Transform QuestionPositions;
+    public GameObject QuestionInput;
+
+
+    public void Start()
+    {
+        LoadQuestions();
+    }
+    public void LoadQuestions()
+    {
+        List<Question> Questions;
+        List<Answer> Answers;
+        Answers = new List<Answer>
+        {
+            new Answer("name", false),
+            new Answer("yolo", true),
+            new Answer("ding", false)
+        };
+
+        Questions = new List<Question>
+        {
+            new Question("wat denk jij vandaag?", Answers),
+            new Question("hoe praat jij tegen vrouwen?", Answers),
+            new Question("Wat is het weer vandaag?", Answers)
+        };
+
+        foreach (Question question in Questions)
+        {
+            GameObject QuestionClone = Instantiate(QuestionInput, QuestionPositions.position, Quaternion.identity);
+            QuestionClone.transform.SetParent(QuestionPositions);
+            QuestionClone.GetComponent<QuestionManager>().questionTitle.text = question.question;
+
+        } 
+
+
+
+    }
+}
