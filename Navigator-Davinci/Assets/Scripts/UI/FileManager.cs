00c57a5f01075be91c1bb84ed79a139bce19f3b1
CodeerBeer <99053182@mydavinci.nl>
2021-01-05 11:49:25 +0100

    Added image to question

---
+++ Navigator-Davinci/Assets/Scripts/UI/FileManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class FileManager : MonoBehaviour
+{
+
+    string Path;
+    public RawImage img;
+
+    void Start()
+    {
+        
+       img.texture = Resources.Load<Texture>("images/defaultimg");
+        
+    }
+    public void OpenExplorer()
+    {
+        Path = EditorUtility.OpenFilePanel("overwrite with png", "", "png");
+        GetImage();
+    }
+
+    public void GetImage()
+    {
+        if(Path != null)
+        {
+            UpdateImage();
+        }
+    }
+
+    public void UpdateImage()
+    {
+        WWW www = new WWW("file:///" + Path);
+        img.texture = www.texture;
+    }
+}
