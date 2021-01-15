878ad0eab77755e1a768cd1876163c51390ad392
CodeerBeer <99053182@mydavinci.nl>
2021-01-15 12:39:27 +0100

    scrolling completed

--- Navigator-Davinci/Assets/Scripts/UI/PuzzleOverview.cs
+++ Navigator-Davinci/Assets/Scripts/UI/PuzzleOverview.cs
@@ -8,7 +8,7 @@ public class PuzzleOverview : MonoBehaviour
     public Transform container;
     public GameObject puzzleObject;
     public static PuzzleOverview instance;
-    private List<Puzzle> puzzles = new List<Puzzle> {new Puzzle(0, "Puzzle 1", null, "easy"), new Puzzle(1, "Puzzle 2", null, "medium"), new Puzzle(3, "Puzzle 3", null, "hard") };
+    private List<Puzzle> puzzles = new List<Puzzle> {new Puzzle(0, "Puzzle 1", null, "easy"), new Puzzle(1, "Puzzle 2", null, "medium"), new Puzzle(3, "Puzzle 3", null, "hard"), new Puzzle(0, "Puzzle 1", null, "easy"), new Puzzle(1, "Puzzle 2", null, "medium"), new Puzzle(3, "Puzzle 3", null, "hard"), new Puzzle(0, "Puzzle 1", null, "easy"), new Puzzle(1, "Puzzle 2", null, "medium"), new Puzzle(3, "Puzzle 3", null, "hard") };
 
     private void Start()
     {
@@ -17,13 +17,13 @@ public class PuzzleOverview : MonoBehaviour
 
     public void LoadPuzzles()
     {
-        if(puzzles.Count > 0)
+        if(puzzles.Count > 0 && puzzles != null)
         {
             foreach(Puzzle puzzle in puzzles)
             {
-                GameObject copy = Instantiate(puzzleObject, container.position, Quaternion.identity);
-                copy.transform.SetParent(container);
-                copy.GetComponent<PuzzleData>().name.text = puzzle.name;
+                GameObject puzzleCopyObject = Instantiate(puzzleObject, container.position, Quaternion.identity);
+                puzzleCopyObject.transform.SetParent(container);
+                puzzleCopyObject.GetComponent<PuzzleData>().name.text = puzzle.name;
             }
         }
     }
