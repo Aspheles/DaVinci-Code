a3961324d1eab86da18cdd453fa827027a876524
CodeerBeer <99053182@mydavinci.nl>
2021-02-05 21:32:30 +0100

    Player can walk through the lobby.
    The animation technicalities are still in development.
    
    Player movement must be further optimized.
    
    Wall collision is absent, to be worked on.

---
+++ Navigator-Davinci/Assets/Scripts/GameCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+
+    Vector3 rotation;
+    Vector3 scale;
+
+    void Start()
+    {
+        rotation = transform.eulerAngles;
+        scale = transform.localScale;
+    }
+    void Move()
+    {
+        transform.GetChild(0).LookAt(target.transform);
+
+        scale.x = Input.GetAxis("Mouse ScrollWheel") * 2;
+        scale.y = Input.GetAxis("Mouse ScrollWheel") * 2;
+        scale.z = Input.GetAxis("Mouse ScrollWheel") * 2;
+
+        rotation.y += Input.GetAxis("Mouse X") * 10;
+        rotation.x -= Input.GetAxis("Mouse Y") * 10;
+
+        transform.eulerAngles = rotation;
+        transform.localScale += scale;
+    }
+    void Update()
+    {
+
+        Move();
+        
+    }
+}
---
+++ Navigator-Davinci/Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject figure;
+    [SerializeField] private GameCamera camera;
+    private Animator animation;
+
+    private void Start()
+    {
+        animation = figure.GetComponent<Animator>();
+    }
+    
+    public void Animate(bool status)
+    {
+        if(status)
+        {
+            animation.SetBool("Idle", false);
+            animation.SetBool("Running", true);
+        }
+        else
+        {
+            animation.SetBool("Running", false);
+            animation.SetBool("Idle", true);
+        }
+        
+    }
+    public void Move(float speed)
+    {
+        if(Input.GetKey(KeyCode.LeftControl))
+        {
+            speed += 5;
+        }
+
+        if (Input.GetKey("w"))
+        {
+            Animate(true);
+            transform.position += new Vector3(camera.transform.forward.x, 0, camera.transform.forward.z) * Time.deltaTime * speed;
+            transform.eulerAngles = Vector3.RotateTowards(transform.eulerAngles, new Vector3(0, camera.transform.GetChild(0).eulerAngles.y, 0), 1, 2.5f);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            Animate(true);
+            transform.position += new Vector3(camera.transform.right.x, 0, camera.transform.right.z) * Time.deltaTime * speed;
+            transform.eulerAngles = Vector3.RotateTowards(transform.eulerAngles, new Vector3(0, camera.transform.GetChild(0).eulerAngles.y + 90, 0), 1, 2.5f);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            Animate(true);
+            transform.position -= new Vector3(camera.transform.forward.x, 0, camera.transform.forward.z) * Time.deltaTime * speed;
+            transform.eulerAngles = Vector3.RotateTowards(transform.eulerAngles, new Vector3(0, camera.transform.GetChild(0).eulerAngles.y + 180 , 0), 1, 2.5f);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            Animate(true);
+            transform.position -= new Vector3(camera.transform.right.x, 0, camera.transform.right.z) * Time.deltaTime * speed;
+            transform.eulerAngles = Vector3.RotateTowards(transform.eulerAngles, new Vector3(0, camera.transform.GetChild(0).eulerAngles.y + -90, 0), 1, 2.5f);
+        }
+
+        else
+        {
+            Animate(false);
+        }
+    }
+
+    void Update()
+    {
+        Move(7);
+    }
+}
