9b67c00d820f84cb0feeebb562efaff152c55bfc
yavuz <yavuzkaand@hotmail.com>
2021-02-18 13:12:41 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/Sprint3-Feature-8-12' into fet-test-4-5-6-7""
    This reverts commit 58e97b4c3d5c6c8a4d817d358b8346ff786179f7.

---
+++ Navigator-Davinci/Assets/Scripts/Portal.cs
@@ -0,0 +1,64 @@
+﻿using NUnit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] GameObject spawnPoint;
+    [SerializeField] GameObject nextPortal;
+
+    [SerializeField] Environment leave;
+    [SerializeField] Environment enter;
+
+    [SerializeField] public GameObject confirm;
+    Player player;
+
+    bool status;
+
+    private void Start()
+    {
+        confirm.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Player>())
+        {
+            player = collision.gameObject.GetComponent<Player>();
+            confirm.SetActive(true);
+
+            UnityEngine.Cursor.visible = true;
+            UnityEngine.Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+
+    public void CancelNow()
+    {
+        confirm.SetActive(false);
+        player.transform.position = spawnPoint.transform.position + new Vector3(0, 1.5f, 0);
+    }
+
+    public void LoadNow()
+    {
+        confirm.SetActive(false);
+
+        Player plr = player.gameObject.GetComponent<Player>();
+        //leave.DespawnPlayer(plr);
+        AsyncOperation loading = SceneManager.LoadSceneAsync(enter.name);
+        //enter.SpawnPlayer(enter.spawnPoint);
+        StartCoroutine(LoadNextScene(loading, enter));
+
+    }
+
+
+
+    private IEnumerator LoadNextScene(AsyncOperation loading, Environment env)
+    {
+        while (loading.progress < 0.9f)
+        {
+            yield return null;
+        }
+        env.SpawnPlayer(env.spawnPoint);
+    }
+}
