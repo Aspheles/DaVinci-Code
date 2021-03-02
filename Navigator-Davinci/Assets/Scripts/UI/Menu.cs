11a93b23e81f9de0b0a1226e52e025aef79075f9
yavuz <yavuzkaand@hotmail.com>
2021-03-02 10:33:27 +0100

    Revert "added models"
    This reverts commit 1a14f4a9e8cb7734e58c0da16d787b481eec2f34.

---
+++ Navigator-Davinci/Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public string menuName;
+    public bool open;
+
+    /// <summary>
+    /// Opens the menu.
+    /// </summary>
+
+    public void Open()
+    {
+        open = true;
+        gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// Closes the menu.
+    /// </summary>
+    public void Close()
+    {
+        open = false;
+        gameObject.SetActive(false);
+    }
+    
+}
---
+++ Navigator-Davinci/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] Menu[] menus;
+    public static MenuManager instance;
+
+    private void Start()
+    {
+        if (instance != null) return;
+        instance = this;
+    }
+
+    /// <summary>
+    /// Opens the specified menu, and closes the rest.
+    /// </summary>
+    /// <param name="menuName"></param>
+    public void OpenMenu(string menuName)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].menuName == menuName)
+            {
+                menus[i].Open();
+            }
+            else if (menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Closes all the menus, that are not open. checks for the current menu and sets it active.
+    /// </summary>
+    /// <param name="menu"></param>
+    public void OpenMenu(Menu menu)
+    {
+        for(int i = 0; i < menus.Length; i++)
+        {
+            if(menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+        menu.Open();
+    }
+
+
+    public void CloseMenu(Menu menu)
+    {
+        menu.Close();
+    }
+}
